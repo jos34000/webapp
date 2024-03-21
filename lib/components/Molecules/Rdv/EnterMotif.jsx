f6dd1422d602fb2ae86a7c16ebf86325fefe6267
jos34000 <jocelynsainson@icloud.com>
2024-03-21 12:07:12 +0100

    Mise à jour des URL de l'API

--- lib/components/Molecules/Rdv/EnterMotif.jsx
+++ lib/components/Molecules/Rdv/EnterMotif.jsx
@@ -13,6 +13,7 @@ export default function EnterMotif({ onMotifChange, motif }) {
       ></label>
       <input
         value={motif}
+        placeholder="motif"
         onChange={handleMotifChange}
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
       />
