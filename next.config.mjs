29a093dbd50f016084c00fe1b0b5efda517e1450
jos34000 <jocelynsainson@icloud.com>
2024-03-28 12:02:35 +0100

    Ajouter la configuration de l'environnement dans next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { config } from "dotenv"
+import { resolve } from "path"
 
-export default nextConfig;
+let envFile
+switch (process.env.NODE_ENV) {
+  case "dev":
+    envFile = ".env.dev"
+    break
+  case "home":
+    envFile = ".env.home"
+    break
+  default:
+    envFile = ".env.prod"
+}
+
+config({ path: resolve("/webapp", envFile) })
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+export default nextConfig
